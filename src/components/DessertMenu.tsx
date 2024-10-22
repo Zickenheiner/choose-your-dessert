12a0b630f92c616fcb6faec53e5d74f77c135bc8
Hakkenheiner <remi.zicken@gmail.com>
2024-10-22 22:29:51 +0200

    create the components DessertMenu and MenuItem

---
+++ src/components/DessertMenu.tsx
@@ -0,0 +1,13 @@
+import data from "../data.json";
+import MenuItem from "./MenuItem";
+
+export default function DessertMenu() {
+  return (
+    <div>
+      <h1>Desserts</h1>
+      {data.map((item) => (
+        <MenuItem item={item} key={item.name} />
+      ))}
+    </div>
+  );
+}
---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,17 @@
+interface Item {
+  item: {
+    image: {
+      thumbnail: string;
+      mobile: string;
+      tablet: string;
+      desktop: string;
+    };
+    name: string;
+    category: string;
+    price: number;
+  };
+}
+export default function MenuItem({ item }: Item) {
+  const { image, name, category, price } = item;
+  return <div>{name}</div>;
+}
